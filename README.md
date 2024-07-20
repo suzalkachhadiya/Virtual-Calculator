d219b110ccf376ebe8785fd9fcf6f3a926e16e98
suzal kachhadiya <suzalkachhadiya111@gmail.com>
2024-07-20 19:19:42 +0530

    README file added

---
+++ README.md
@@ -0,0 +1,80 @@
+## Virtual Calculator
+
+<details open="open">
+  <summary>Table of Contents</summary>
+  <ol>
+    <li>
+      <a href="#Technologies">Technologies</a>
+    </li>
+    <li>
+      <a href="#Example">Example</a>
+    </li>
+    <li>
+	    <a href = "#How-to-Start-Project">How to Start Project</a>
+    </li>
+    <li>
+	    <a href = "#Contact-Us">Contact Us</a>
+    </li>
+    <li>
+	    <a href = "#License">License</a>
+    </li>
+  </ol>
+</details>
+
+## Technologies
+- Python
+- opencv-python
+- cvzone
+- mediapipe
+
+## Example:
+![Video Name](./assets/Virtual_calc.mp4)
+
+## How to Start Project
+
+Follow these steps to get started with the project:
+
+1. **Clone the Repository:**
+   ```bash
+   git clone <repository_link>
+   ```
+2. **Install Anaconda:**
+   
+   Make sure you have Anaconda installed on your system. If not, you can download and install it from the official website: https://www.anaconda.com/download/
+   
+4. **Create a Virtual Environment:**
+   
+   Create a new virtual environment using Python 3.8:
+
+   ```bash
+   conda create --name your_env_name python=3.8 -y
+   ```
+   Replace your_env_name with the desired name for your virtual environment.
+   
+   Activate the newly created environment:
+   ```bash
+   conda activate your_env_name
+   ```
+5. **Install Dependencies:**
+   
+   Install the project dependencies by running:
+   ```bash
+   pip install -r requirements.txt
+   ```
+   This command will install all the required packages listed in the requirements.txt file.
+
+7. **Run the Calculator App:**
+   ```bash
+   main.py
+   ```
+   This command will start the virtual Calculator.
+
+## Contact Us
+
+To learn more about, please reach out:
+
+📧 suzalkachhadiya111@gmail.com
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
--- main.py
+++ main.py
@@ -1,113 +1,115 @@
 import cv2
+import streamlit as st
 from cvzone.HandTrackingModule import HandDetector
 
-class Button:
-    def __init__(self,pos,width,height,value):
-        self.pos=pos
-        self.width=width
-        self.height=height
-        self.value=value
+st.title("Virtual Calculator")
 
-    def draw(self,img):
-        cv2.rectangle(img,self.pos,(self.pos[0]+self.width,self.pos[1]+self.height),
-                      (225,225,255),cv2.FILLED)
-        cv2.rectangle(img,self.pos,(self.pos[0]+self.width,self.pos[1]+self.height),
-                      (45,45,45),3)
-        cv2.putText(img,self.value,(self.pos[0]+33,self.pos[1]+50),cv2.FONT_HERSHEY_PLAIN,
-                    2,(0,0,0),2)
-        
-    def checkClick(self,x,y):
-        if self.pos[0] < x < self.pos[0]+self.width and \
-                self.pos[1] < y < self.pos[1]+self.height:
+class Button:
+        def __init__(self,pos,width,height,value):
+            self.pos=pos
+            self.width=width
+            self.height=height
+            self.value=value
+
+        def draw(self,img):
             cv2.rectangle(img,self.pos,(self.pos[0]+self.width,self.pos[1]+self.height),
-                      (225,225,255),cv2.FILLED)
+                        (225,225,255),cv2.FILLED)
             cv2.rectangle(img,self.pos,(self.pos[0]+self.width,self.pos[1]+self.height),
                         (45,45,45),3)
-            cv2.putText(img,self.value,(self.pos[0]+15,self.pos[1]+70),cv2.FONT_HERSHEY_PLAIN,
-                        5,(0,0,0),5)
-            return True
-        return False
+            cv2.putText(img,self.value,(self.pos[0]+33,self.pos[1]+50),cv2.FONT_HERSHEY_PLAIN,
+                        2,(0,0,0),2)
+            
+        def checkClick(self,x,y):
+            if self.pos[0] < x < self.pos[0]+self.width and \
+                    self.pos[1] < y < self.pos[1]+self.height:
+                cv2.rectangle(img,self.pos,(self.pos[0]+self.width,self.pos[1]+self.height),
+                        (225,225,255),cv2.FILLED)
+                cv2.rectangle(img,self.pos,(self.pos[0]+self.width,self.pos[1]+self.height),
+                            (45,45,45),3)
+                cv2.putText(img,self.value,(self.pos[0]+15,self.pos[1]+70),cv2.FONT_HERSHEY_PLAIN,
+                            5,(0,0,0),5)
+                return True
+            return False
 
 cap=cv2.VideoCapture(0)
 cap.set(3,1080) # width
 cap.set(4,480) # height
 detector=HandDetector(detectionCon=0.7, maxHands=1)
 
-buttonValues=[['(',')','E','C'],
-              ['7','8','9','*'],
-              ['4','5','6','+'],
-              ['1','2','3','-'],
-              ['0','.','=','/'],]
+buttonValues=[  ['E','7','8','9','*'],
+                ['C','4','5','6','+'],
+                ['(','1','2','3','-'],
+                [')','.','0','=','/'],]
 
 buttonnList=[]
-for i in range(4):
-    for j in range(5):
-        xpos=i*100 + 500
-        ypos=j*100 + 190
+for i in range(5):
+    for j in range(4):
+        xpos=i*100 + 600
+        ypos=j*100 + 170
         buttonnList.append(Button((xpos,ypos),80,80,buttonValues[j][i]))
 
-#varables
+#variables
 myEquation=""
 delayCounter=0
 
 while True:
-    try:
-        #get image from webcam
-        success, img= cap.read()
+        try:
+            #get image from webcam
+            success, img= cap.read()
 
-        if not success:
-                print("Error: Failed to read frame from webcam.")
-                break
-        
-        img=cv2.flip(img, 1)
+            if not success:
+                    print("Error: Failed to read frame from webcam.")
+                    break
+            
+            img=cv2.flip(img, 1)
 
-        #Detection of hand
-        hands, img=detector.findHands(img,flipType=False)
+            #Detection of hand
+            hands, img=detector.findHands(img,flipType=False)
 
-        # Draw all button
-        cv2.rectangle(img,(500,50),(500+383,80+80),
-                        (225,225,255),cv2.FILLED)
-        cv2.rectangle(img,(500,50),(500+383,80+80),
-                        (45,45,45),3)
-        for button in buttonnList:
-            button.draw(img)
+            # Draw all button
+            cv2.rectangle(img,(600,50),(500+580,80+80),
+                            (225,225,255),cv2.FILLED)
+            cv2.rectangle(img,(600,50),(500+580,80+80),
+                            (45,45,45),3)
+            for button in buttonnList:
+                button.draw(img)
 
-        # chech for hand
-        if hands:
-            lmlist=hands[0]['lmList']
-            length, _, img = detector.findDistance(lmlist[8][:2],lmlist[12][:2],img)
-            # print(length)
-            x,y=lmlist[8][:2]
-            if length<50:
-                for i, button in enumerate(buttonnList):
-                    if button.checkClick(x,y) and delayCounter==0:
-                        myValues=buttonValues[int(i%5)][int(i/5)]
-                        print(i,"->",myValues)
-                        if myValues== "=":
-                            myEquation=str(eval(myEquation))
-                        elif myValues=="E":
-                            myEquation=myEquation[:-1] if myEquation else myEquation
-                            # print(myEquation)
-                        elif myValues=="C":
-                            myEquation=""
-                        else:
-                            myEquation+=myValues
-                        delayCounter=1
-        
-        # Avoid Duplicates
-        if delayCounter!=0:
-            delayCounter+=1
-            if delayCounter>10:
-                delayCounter=0
+            # chech for hand
+            if hands:
+                lmlist=hands[0]['lmList']
+                length, _, img = detector.findDistance(lmlist[8][:2],lmlist[12][:2],img)
+                # print(length)
+                x,y=lmlist[8][:2]
+                if length<50:
+                    for i, button in enumerate(buttonnList):
+                        if button.checkClick(x,y) and delayCounter==0:
+                            myValues=buttonValues[int(i%4)][int(i/4)]
+                            # print(i,"->",myValues)
+                            if myValues== "=":
+                                myEquation=str(eval(myEquation))
+                            elif myValues=="E":
+                                myEquation=myEquation[:-1] if myEquation else myEquation
+                                # print(myEquation)
+                            elif myValues=="C":
+                                myEquation=""
+                            else:
+                                myEquation+=myValues
+                            delayCounter=1
+            
+            # Avoid Duplicates
+            if delayCounter!=0:
+                delayCounter+=1
+                if delayCounter>10:
+                    delayCounter=0
 
-        # Display the result / equation
-        cv2.putText(img,myEquation,(510,120),cv2.FONT_HERSHEY_PLAIN,
-                        3,(40,40,40),3)
+            # Display the result / equation
+            cv2.putText(img,myEquation,(610,120),cv2.FONT_HERSHEY_PLAIN,
+                            3,(40,40,40),3)
 
-        #display Image
-        cv2.imshow("Image",img)
-        cv2.waitKey(1)
+            #display Image
+            cv2.imshow("Image",img)
+            cv2.waitKey(1)
 
-    except Exception as e:
-        print(f"An error occurred: {str(e)}")
-        break
+        except Exception as e:
+            print(f"An error occurred: {str(e)}")
+            break
